ea6000d48f746f98c00290c54861672bbc6053d9
Leandro Sousa Costa <118322766+LeannonS@users.noreply.github.com>
2025-05-18 15:55:31 -0300

    Update equal.pas

--- examples/equal.pas
+++ examples/equal.pas
@@ -1,22 +1,22 @@
 program equal;
 
 var
-  a, b: interger;
-  r: real = 3.;
+  a, b: integer;
+  r: real;
 
 begin
   a := 25 div 03;
   b := a / 0x02;
 
   if (a = b) then
-    write ("that's it");
+    write ("that's\tit");
 
   else if (a == b) then
     write ("ok");
 
-  else if (a === b) then
+  else if (a = b) then
     write ("arigato");
 
-  else if (a ==== b) then
+  else if (a == b) then
     write ("rouje");
 end.
